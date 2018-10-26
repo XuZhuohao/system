8a9d0a3e9a4ba631b77eb3caf844ec67afcfb691
XuZhuohao <786725551@qq.com>
2018-10-26 14:41:42 +0800

    修改为springCloud程序

--- my-email/src/main/java/com/yui/system/myemail/MyEmailApplication.java
+++ my-email/src/main/java/com/yui/system/myemail/MyEmailApplication.java
@@ -2,8 +2,9 @@ package com.yui.system.myemail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
+@EnableEurekaClient
 @SpringBootApplication
 @EnableJpaAuditing
 public class MyEmailApplication {
